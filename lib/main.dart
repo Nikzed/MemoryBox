1d1e3072def7adf5dd39f68b093297d6bc986100
Nized <nik.shadkov@mail.ru>
2021-09-25 20:58:34 +0300

    Test Auto Commit

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,7 @@ void main() async{
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
   runApp(MyApp());
+  print('check changes');
 }
 
 class MyApp extends StatelessWidget {
--- test.sh
+++ test.sh
@@ -1,3 +1,4 @@
+/bin/bash -c "sleep 15 && while true; do <C:/important/StudioProjects/MemoryBox/test.sh> ; sleep 30; done"
 cd C:/important/StudioProjects/MemoryBox
 git add .
 git commit -m 'Test Auto Commit'
