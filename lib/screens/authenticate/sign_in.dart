03c04b979755dc2575c1d3350c7931d5ab242480
Nikzed <nik.shadkov@mail.ru>
2021-10-11 14:20:12 +0300

    login changed

--- lib/screens/authenticate/sign_in.dart
+++ lib/screens/authenticate/sign_in.dart
@@ -1,7 +1,7 @@
 import 'package:first_project_test/model/painter_model.dart';
 import 'package:first_project_test/services/auth_service.dart';
 import 'package:flutter/material.dart';
-
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class SignIn extends StatefulWidget {
   const SignIn({Key? key}) : super(key: key);
@@ -15,66 +15,80 @@ class _SignInState extends State<SignIn> {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(fontFamily: 'Roboto'),
-      home: Scaffold(
-        backgroundColor: Colors.white,
-        body: Stack(children: [
-          Container(
-            width: 500,
-            height: 400,
-            child: CustomPaint(
-              painter: CirclePainter(),
+    return ScreenUtilInit(
+      builder: () => MaterialApp(
+        theme: ThemeData(fontFamily: 'Roboto'),
+        home: Scaffold(
+          backgroundColor: Colors.white,
+          body: Stack(children: [
+            Container(
+              width: 500,
+              height: 400,
+              child: CustomPaint(
+                painter: CirclePainter(),
+              ),
             ),
-          ),
-          Container(
-            margin: EdgeInsets.only(left: 60, top: 157, right: 59),
-            width: 295,
-            height: 67,
-            child: Stack(
-              children: [
-                Text('MemoryBox',
-                  style: TextStyle(color: Colors.white, fontSize: 48),
-                ),
-                Align(
-                  alignment: Alignment.bottomRight,
-                  child: Text('Твой голос всегда рядом',
-                    style: TextStyle(color: Colors.white, fontSize: 14),
+            Container(
+              margin: EdgeInsets.symmetric(vertical: 157.h, horizontal: 67.w),
+              child: Container(
+                child: Column(children: [
+                  Text(
+                    'MemoryBox',
+                    style: TextStyle(color: Colors.white, fontSize: 48),
                   ),
-                )
-              ],
+                  Align(
+                    alignment: Alignment.bottomRight,
+                    child: Text(
+                      'Твой голос всегда рядом',
+                      style: TextStyle(color: Colors.white, fontSize: 14),
+                    ),
+                  )
+                ]),
+              ),
             ),
-          ),
-          Container(
-            alignment: Alignment.topCenter,
-            padding: EdgeInsets.only(top: 350.0),
-            child: Column(
-              children: [
-                Text('data'),
-                Text('sadadgood')
-              ],
+            Align(
+              alignment: Alignment.center,
+              child: Container(
+                width: 200.w,
+                height: 57.h,
+                margin: EdgeInsets.only(top: 100.h),
+                alignment: Alignment.center,
+                child: Text('data'),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  shape: BoxShape.rectangle,
+                  borderRadius: BorderRadius.all(Radius.circular(15.0)),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.shade400,
+                      blurRadius: 10,
+                      offset: Offset(0, 2), // Shadow position
+                    ),
+                  ],
+                ),
+              ),
             ),
-          ),
-          Container(
-            alignment: Alignment.topCenter,
-            padding: EdgeInsets.only(top: 500.0),
-            child: ElevatedButton(
-              child: Text('Sign in anon'),
-              style: ButtonStyle(
-                  backgroundColor: MaterialStateProperty.all(Colors.pink)),
-              onPressed: () async {
-                dynamic result = await _auth.signInAnon();
+            Container(
+              alignment: Alignment.topCenter,
+              padding: EdgeInsets.only(top: 500.0),
+              child: ElevatedButton(
+                child: Text('Sign in anon'),
+                style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all(Colors.pink)),
+                onPressed: () async {
+                  dynamic result = await _auth.signInAnon();
 
-                if (result == null) {
-                  print('error signing in');
-                } else {
-                  print('signed in');
-                  return result.uid;
-                }
-              },
+                  if (result == null) {
+                    print('error signing in');
+                  } else {
+                    print('signed in');
+                    return result.uid;
+                  }
+                },
+              ),
             ),
-          ),
-        ]),
+          ]),
+        ),
       ),
     );
   }
